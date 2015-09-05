bd8ef64c84de0e8d7b647dc3e12cc2724e89bc34
Gowtham Ashok <gwty93@gmail.com>
2015-09-05 01:31:05 -0400

    Got balance from capitalOne API

--- app/capitalData.py
+++ app/capitalData.py
@@ -4,4 +4,12 @@ import urllib2
 import urllib
 import argparse
 import sys
-import oauth2
+#import oauth2
+
+url = "http://api.reimaginebanking.com/customers/55e94a6af8d8770528e60de3/accounts?key=779198b647652d0c1ff6836c713703ff"
+
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+
+print data
+print data[0]["balance"]
