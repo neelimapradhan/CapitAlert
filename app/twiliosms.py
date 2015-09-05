2b26a4d873e178c515d223618da5842a6d721af6
neelimapradhan <neelimapradhan24@gmail.com>
2015-09-05 01:45:34 -0400

    capital data

---
+++ app/twiliosms.py
@@ -0,0 +1,23 @@
+import twilio
+import twilio.rest
+from twilio.rest import TwilioRestClient
+import capitalData
+
+
+account_sid = "ACc21fbf5413690aa9b013780eb73c722d"
+auth_token = "7e1b5aafb00d3bfa211a7a3fa7707e92"
+twilio_number = "4106566222"
+
+def send_message():
+	name = capitalData.get_Name()
+	desired_item = "hoodie"
+	send_number = "4436949031"
+	message_to_send = "Hello " + name + ", you now have enough money to buy " + desired_item + " yay!!"
+	client = TwilioRestClient(account_sid, auth_token)
+ 	message = client.messages.create(body=message_to_send,
+    to=send_number,    # your phone number
+    from_= twilio_number) # your Twilio number
+	print message.sid
+
+
+send_message()
