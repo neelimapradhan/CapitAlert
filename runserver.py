6f27112bf00acdd9a42646da2025f63b21666ae2
neelimapradhan <neelimapradhan24@gmail.com>
2015-09-05 14:37:16 -0400

    gathering data

--- runserver.py
+++ runserver.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
-
+from app import capitalData
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return "hello world"
+	name = capitalData.get_Name()
+	return render_template('index.html', name=name)
 
 if __name__ == "__main__":
 	app.run(debug=True)
