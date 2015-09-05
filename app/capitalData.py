20af1d8f518a521e2b791c3480fa58bd5fcc2b77
Gowtham Ashok <gwty93@gmail.com>
2015-09-05 01:37:23 -0400

    added methods

--- app/capitalData.py
+++ app/capitalData.py
@@ -4,12 +4,19 @@ import urllib2
 import urllib
 import argparse
 import sys
-#import oauth2
 
-url = "http://api.reimaginebanking.com/customers/55e94a6af8d8770528e60de3/accounts?key=779198b647652d0c1ff6836c713703ff"
+
+testkey = "779198b647652d0c1ff6836c713703ff"
+url = "http://api.reimaginebanking.com/customers/55e94a6af8d8770528e60de3/accounts?key="+ testkey
 
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 
-print data
-print data[0]["balance"]
+def get_Balance():	
+  return data[0]["balance"]
+
+def get_Name():
+  return data[0]["nickname"]
+
+def get_account_type():
+  return data[0][""]
